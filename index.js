b101d4fc4e928fa81f8fc6dfdc0548e8b7ac097d
Dick H Larsson <dicklarsson@gmail.com>
2022-10-14 16:42:35 +0200

    Enhance documentation

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ const port = process.env.PORT || 80;
 
 app.use(cookieParser());
 
+/* start page */
 app.get("/", startpage);
 
-app.get("/secure", secure);
-
+/* sign in page has two "states", 
+1) When the user wants to sign in (no orderRef query parameter)
+2) We are redirected back from the Identity Provider, we have a orderRef query parameter
+*/
 app.get("/signin", async (req, res) => {
   if (req.query.orderRef) {
     authenticate(req, res);
@@ -24,6 +27,10 @@ app.get("/signin", async (req, res) => {
   }
 });
 
+/* secure - when the user has signed in */
+app.get("/secure", secure);
+
+/* well, sign out, clear the session cookie */
 app.get("/signout", function (req, res) {
   res.cookie("sessionCookie", null);
   res.redirect("/");
