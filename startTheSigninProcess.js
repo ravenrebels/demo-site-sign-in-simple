8f8d91ea0f6f9d7a4cd6aca432daebfd72ca4a2e
Dick H Larsson <dicklarsson@gmail.com>
2022-10-14 16:22:09 +0200

    Fix redirect URL

--- startTheSigninProcess.js
+++ startTheSigninProcess.js
@@ -2,12 +2,18 @@ const axios = require("axios");
 
 async function startTheSigninProcess(req, res) {
   //Create an authentication/sign order using Raven Rebels Identity Provider
+
+  //since we are redirected here we have to add the /signin part
+  const redirectURL = (req.get("Referer") + "/signin").replace(
+    "//signin",
+    "/signin"
+  ); //In case we get double / just replace //signin with /signin
   const payload = {
     userVisibleData: Buffer.from(
       "Sign in to Demo site using Ravencoin NFT " + new Date()
     ).toString("base64"),
     endUserIp: "127.0.0.1",
-    redirectURL: req.get("Referer"),
+    redirectURL,
   };
   const { data } = await axios.post(
     "https://idp.ravenrebels.com/rp/v5.1/sign",
